ae592e47cdeabdb287193ee3273b4745d32829aa
nanukashubladze <123475291+nanukashubladze@users.noreply.github.com>
2024-10-23 14:52:01 +0400

    signup done

---
+++ src/signup/Signup.jsx
@@ -0,0 +1,39 @@
+import './signup.css';
+
+const Signup = () => {
+  return (
+    <div className='addUser'>
+      <h3>Sign Up</h3>
+      <form className='addUserForm'>
+        <div className='inputGoup'>
+          <label htmlFor='name' >Name:</label>
+          <input 
+          type="text"
+          id='name'
+          autoComplete='off'
+          placeholder='Enter your name'
+           />
+             <label htmlFor='email' >Email:</label>
+          <input 
+          type="email"
+          id='email'
+          autoComplete='off'
+          placeholder='Enter your email'
+           />
+             <label htmlFor='password' >Password:</label>
+          <input 
+          type="password"
+          id='password'
+          autoComplete='off'
+          placeholder='Enter password'
+           />
+           <button type="submit" className="btn btn-success">
+            Sign Up
+            </button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Signup
